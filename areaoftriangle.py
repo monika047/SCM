84d30490d124a99467c93429d19ed83779bd19a1
Monika G <monikaggopalappa@gmail.com>
2025-03-18 21:36:05 +0530

    third commit

--- areaoftriangle.py
+++ areaoftriangle.py
@@ -7,4 +7,6 @@ c = 7
 # a = float(input('Enter first side: '))
 # b = float(input('Enter second side: '))
 # c = float(input('Enter third side: '))
+# calculate the semi-perimeter
+s = (a + b + c) / 2
 

181a2d3fa53ccfd72d818e2ebf40aa5cc10be57b
Monika G <monikaggopalappa@gmail.com>
2025-03-19 13:56:59 +0530

    print statement

---
+++ 19thMarch/swap2no.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+

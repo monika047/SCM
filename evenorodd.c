0c3db1853599f85023582946d1a9d9e3dca4f504
Monika G <monikaggopalappa@gmail.com>
2025-03-19 13:11:23 +0530

    print statement

--- evenorodd.c
+++ evenorodd.c
@@ -3,3 +3,11 @@ int main() {
 	int num;
         printf("enter an integer:");
 	scanf("%d",&num);
+	if(num % 2 ==0)
+		printf("%d is even." , num);
+	    else
+        printf("%d is odd.", num);
+
+    return 0;
+}
+

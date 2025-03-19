50b8a84ed45eebe8af18e263a0fd841820c8e4da
Monika G <monikaggopalappa@gmail.com>
2025-03-19 14:17:26 +0530

    print statement

--- 19thMarch/swap2no.c
+++ 19thMarch/swap2no.c
@@ -6,14 +6,8 @@ int main() {
   printf("Enter second number: ");
   scanf("%lf", &second);
   temp = first;
-
-  // value of second is assigned to first
   first = second;
-
-  // value of temp (initial value of first) is assigned to second
   second = temp;
-
-  // %.2lf displays number up to 2 decimal points
   printf("\nAfter swapping, first number = %.2lf\n", first);
   printf("After swapping, second number = %.2lf", second);
   return 0;

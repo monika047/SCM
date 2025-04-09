f841ddd3128bfec0e1a2b13d669a061ef53e85c7
Monika G <monikaggopalappa@gmail.com>
2025-04-09 14:32:14 +0530

    print statement

--- evenorodd.c
+++ evenorodd.c
@@ -5,6 +5,8 @@ int main() {
 	scanf("%d",&num);
 	if(num % 2 ==0)
 		printf("%d is even." , num);
+	        printf("%d is aub");
+		printf("%d is cse");
 	    else
         printf("%d is odd.", num);
 

b1381967900060c17ee3273476c5887d0a12aaf0
Monika G <monikaggopalappa@gmail.com>
2025-05-05 20:09:51 +0530

    c3

--- arr.c
+++ arr.c
@@ -5,5 +5,10 @@ int n, i;
 
 printf("Enter the numbers of elements: ");
     scanf("%d", &n);
+    while (n > 100 || n < 1) {
+        printf("Error! number should in range of (1 to 100).\n");
+        printf("Enter the number again: ");
+        scanf("%d", &n);
+    }
 
 

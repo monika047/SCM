3e7c280306530abba11f8e8fb42d91251fed4de6
Monika G <monikaggopalappa@gmail.com>
2025-05-05 19:56:09 +0530

    c3

--- lcm.c
+++ lcm.c
@@ -5,6 +5,13 @@ int main() {
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
 
+    max = (n1 > n2) ? n1 : n2;
+
+    lcm = max;
+
+    while ((lcm % n1 != 0) || (lcm % n2 != 0)) {
+        lcm += max;
+    }
 
 
 

51671058e3027dfb444c4452ed70c7c87285e905
Monika G <monikaggopalappa@gmail.com>
2025-05-06 20:09:09 +0530

    c3

--- rec.c
+++ rec.c
@@ -4,4 +4,7 @@ int main() {
     int n1, n2;
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
+    printf("G.C.D of %d and %d is %d.", n1, n2, hcf(n1, n2));
+    return 0;
+}
 
